f1989cee7c51c9b1a48fd5c9a3443ed5c764a0b8
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-06 02:09:57 +0100

    Update [id].js

--- pages/api/jsons/[id].js
+++ pages/api/jsons/[id].js
@@ -2,10 +2,11 @@
 
 export default function handler(req, res) {
   const { id } = req.query
+  let id2 = id.substring(1)
   res.status(200).json({
-    "name": "Flow Life #"+id+"",
-    "description": "Flow Life #"+id+" \n by @bobochdbrew",
-    "animation_url": "https://bobochd-brew.github.io/Crypto-Flows-Life-Gen2/index.html?id="+id+"",
+    "name": "Flow Life #"+id2+"",
+    "description": "Flow Life #"+id2+" \n by @bobochdbrew",
+    "animation_url": "https://bobochd-brew.github.io/Crypto-Flows-Life-Gen2/index.html?id="+id2+"",
     "image": "https://bobochd-brew.github.io/Crypto-Flows-Life-Gen2/0.png",
     "creator": "Boboch D. Brew",
     })
