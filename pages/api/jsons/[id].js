8cb59f7fe9f06d33b7d3e682f77ed10c8f872361
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-08-07 07:53:08 +0100

    Update [id].js

--- pages/api/jsons/[id].js
+++ pages/api/jsons/[id].js
@@ -6,6 +6,7 @@ export default function handler(req, res) {
   let id3 = id.split("-")[1];
   let id4 = id.split("-")[2];
   let bina = parseInt(id2).toString(2);
+  let id5 = id.split("-")[3];
   bina = Array.apply(null, Array(id3-bina.length)).map((a) => "0").join("")+bina;
   res.status(200).json({
     "name": "Flow Life G2 #"+id4+"",
@@ -13,5 +14,8 @@ export default function handler(req, res) {
     "animation_url": "https://bobochd-brew.github.io/Crypto-Flows-Life-Gen2/index.html?id="+bina+"",
     "image": "https://bobochd-brew.github.io/Crypto-Flows-Life-Gen2/"+id3+".png",
     "creator": "Boboch D. Brew",
+    "attributes": [
+        { "trait_type": "Breeds", "value": id5 }
+    ]
     })
 }
